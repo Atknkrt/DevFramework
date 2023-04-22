97e67c6335a3da9eb29c726f8727118ab908ffcc
levent3 <lvntkrt333@gmail.com>
2023-04-22 04:40:37 +0300

    cache

--- DevFramework.Core/Aspects/Postsharp/TransactionAspects/TransactionalScopeAspect.cs
+++ DevFramework.Core/Aspects/Postsharp/TransactionAspects/TransactionalScopeAspect.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using PostSharp.Aspects;
+using PostSharp.Aspects.Advices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace DevFramework.Core.Aspects.Postsharp.TransactionAspects
 {
-    internal class TransactionalScopeAspect
+    [Serializable]
+    public class TransactionalScopeAspect:OnMethodBoundaryAspect
     {
+        private TransactionScopeOption _option;
+        public TransactionalScopeAspect()
+        {
+
+        }
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            args.MethodExecutionTag = new TransactionScope(_option);
+        }
+        public override void OnSuccess(MethodExecutionArgs args)
+        {
+            ((TransactionScope)args.MethodExecutionTag).Complete();
+        }
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            ((TransactionScope)args.MethodExecutionTag).Dispose();
+        }
     }
 }
